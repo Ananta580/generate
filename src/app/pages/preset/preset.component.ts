659c9ef5282cbf2b072ec56a9a02cb3a0e084f29
Ananta580 <anantapoudel580@gmail.com>
2024-05-21 11:28:03 +0545

    okok

---
+++ src/app/pages/preset/preset.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'gen-preset',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './preset.component.html',
+  styleUrls: ['./preset.component.css']
+})
+export class PresetComponent {
+
+}
